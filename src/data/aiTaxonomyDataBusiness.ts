6bcff7feb785204c7ecb47016fdb4a12843729c2
TeunvdL <103111282+TeunvdL@users.noreply.github.com>
2025-09-15 13:06:33 +0200

    Revert to Machine learning and AI agents

--- src/data/aiTaxonomyDataBusiness.ts
+++ src/data/aiTaxonomyDataBusiness.ts
@@ -4,8 +4,8 @@ export const aiTaxonomyDataBusiness = {
   "description": "Artificial Intelligence - Technologies that enable machines to simulate human intelligence.",
   "children": [
     {
-      "id": "data",
-      "name": "Data",
+      "id": "Machine Learning",
+      "name": "machine-learning",
       "description": "AI technologies that work with structured data to predict, decide, and optimize.",
       "children": [
         {
@@ -35,8 +35,8 @@ export const aiTaxonomyDataBusiness = {
       ]
     },
     {
-      "id": "text",
-      "name": "Text",
+      "id": "AI Agents",
+      "name": "AI-agents",
       "description": "AI technologies that process and understand textual information.",
       "children": [
         {
